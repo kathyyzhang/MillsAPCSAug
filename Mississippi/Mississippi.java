d414880e93942220f2c8d654765a02fd978773ce
kathyyzhang <kathyzhang168@gmail.com>
2015-08-25 09:26:37 -0700

    Mississippi

---
+++ Mississippi/Mississippi.java
@@ -0,0 +1,65 @@
+
+public class Mississippi
+{
+    public static void PrintM()
+    {
+        System.out.println("M     M");
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println();
+    }
+
+    public static void PrintI()
+    {
+        System.out.println("IIIII");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("IIIII");
+        System.out.println();
+    }
+
+    public static void PrintS()
+    {
+        System.out.println(" SSSSS");
+        System.out.println("S     S");
+        System.out.println("S");
+        System.out.println(" SSSSS");
+        System.out.println("      S");
+        System.out.println("S     S");
+        System.out.println(" SSSSS");
+        System.out.println();
+    }
+
+    public static void PrintP()
+    {
+        System.out.println("PPPPPP");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        PrintM();
+        PrintI();
+        PrintS();
+        PrintS();
+        PrintI();
+        PrintS();
+        PrintS();
+        PrintI();
+        PrintP();
+        PrintP();
+        PrintI();
+    }
+}
