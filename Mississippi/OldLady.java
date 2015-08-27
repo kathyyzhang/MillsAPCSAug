93e690fe00b19f139e549f8146d44af7c2e15c71
kathyyzhang <kathyzhang168@gmail.com>
2015-08-26 20:38:11 -0700

    8.26.15 HW

---
+++ Mississippi/OldLady.java
@@ -0,0 +1,61 @@
+public class OldLady
+{
+    public static void PrintBeg()
+    {
+        System.out.print("There was an old lady who swallowed a ");
+    }
+
+    public static void PrintSpider()
+    {
+        System.out.println("She swallowed the spider to catch the fly");
+    }
+
+    public static void PrintIdk()
+    {
+        System.out.println("I don't know why\nShe swallowed the fly\nPerhaps she'll die.");
+    }
+
+    public static void PrintBird()
+    {
+        System.out.println("She swallowed the bird to catch the spider.");
+    }
+
+    public static void main(String[] args)
+    {
+        PrintBeg();
+        System.out.println("fly.");
+        System.out.println("I don't know why she swallowed the fly");
+        System.out.println("Perhaps she'll die\nBut it's only a fly\nI think I'll cry\nShe gulped it out of the sky\nOh, my!");
+        System.out.println();
+        PrintBeg();
+        System.out.println("spider.");
+        System.out.println("That wiggled and jiggled and tickled inside her.");
+        PrintSpider();
+        PrintIdk();
+        System.out.println("Gone to the by and by\nSigh");
+        System.out.println();
+        PrintBeg();
+        System.out.println("bird.");
+        System.out.println("How absurd! She swallowed a bird!");
+        PrintBird();
+        PrintSpider();
+        PrintIdk();
+        System.out.println("She'll leave us high and dry.");
+        System.out.println();
+        PrintBeg();
+        System.out.println("cat.");
+        System.out.println("Imagine that! She swallowed a cat.");
+        System.out.println("She swallowed the cat to catch the bird.");
+        PrintBird();
+        PrintSpider();
+        PrintIdk();
+        System.out.println("I hope it's a lie.");
+        System.out.println();
+        PrintBeg();
+        System.out.println("horse.\nShe died, of course.\nIt was the last course.\nI'm filled with remorse.\nWhat's left to say...\nEven the artist is crying.");
+        System.out.println("We'll miss her dearly.\nIt is such a loss.\nShe had no time to floss.\nShe missed out on the sauce.");
+        System.out.println();
+        System.out.print("Moral: Never swallow a horse.");
+    }
+}
+
