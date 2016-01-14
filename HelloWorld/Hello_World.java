a5c876ab745e3b071ed5eec1d95c23feb7122843
kathyyzhang <kathyzhang168@gmail.com>
2016-01-14 11:20:06 -0800

    Hello world demonstration 8.18.15
    great second try.

---
+++ HelloWorld/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * This will do a simple print line. First java code. yay.
+ * 
+ * @author Kathy Zhang 
+ * @version August 18,  2015
+ */
+public class Hello_World
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello World! From Kathy");
+    }
+}
